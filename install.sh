6d7b6ebe19c8b295cdd40afad329bb499c31d78a
cyzhou1028 <christopher.y.zhou@duke.edu>
2024-01-07 21:57:19 -0500

    added install script

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+rm ~/.bashrc
+rm -r ~/.config/alacritty
+rm -r ~/.config/awesome
+rm -r ~/.config/picom
+rm -r ~/.config/polybar
+rm -r ~/.config/rofi
+rm ~/.oh-my-bash/themes/powerline/powerline.theme.sh
+
+cp .bashrc ~
+cp -r alacritty ~/.config
+cp -r awesome ~/.config
+cp -r picom ~/.config
+cp -r polybar ~/.config
+cp -r rofi ~/.config
+cp oh_my_bash_powerline_theme_custom ~/.oh-my-bash/themes/powerline/
+mv ~/.oh-my-bash/themes/powerline/oh_my_bash_powerline_theme_custom ~/.oh-my-bash/themes/powerline/powerline.theme.sh

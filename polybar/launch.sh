ef4bf8353249633d15d7f08654ae962765afbd68
cyzhou1028 <chrisopher.y.zhou@duke.edu>
2024-03-22 21:06:49 -0400

    polybar multimonitor

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,7 +1,8 @@
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload example &
+    MONITOR=$m polybar --reload main &
+    polybar --reload secondary &
   done
 else
-  polybar --reload example &
+  polybar --reload main &
 fi
